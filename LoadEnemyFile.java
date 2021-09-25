10d8337e724442babc2494329db475c63f3b3186
Rae-Zou <76859781+Rae-Zou@users.noreply.github.com>
2021-09-25 14:11:39 +1200

    Create LoadEnemyFile.java

---
+++ LoadEnemyFile.java
@@ -0,0 +1,68 @@
+public class LoadEnemyFile {
+
+    private List<Enemy> enemyClasses = new ArrayList<>(); // store all bugs
+
+    /**
+     * get all the Enemy classes from jar.
+     *
+     * @return a list of classes
+     */
+    public List<Enemy> getEnemyClasses(){
+        return this.enemyClasses;
+    }
+
+    /**
+     * Load all enemy from the jar file.
+     * construct all enemy objects with associated positions.
+     * store them in enemyClasses list.
+     *
+     * @param pos list of pos
+     * @throws MalformedURLException msg
+     * @throws ClassNotFoundException msg
+     * @throws IllegalAccessException msg
+     * @throws InstantiationException msg
+     */
+    public void setEnemyClasses(List<Coordinate> pos, String name) throws MalformedURLException,
+            ClassNotFoundException, IllegalAccessException, InstantiationException {
+        File jarFile = new File("src/nz/ac/vuw/ecs/swen225/gp21/persistency/levels");
+        File[] files = jarFile.listFiles(file -> file.getPath().toLowerCase().endsWith(".jar"));
+
+        assert files != null;
+
+        Class classToLoad = Class.forName(name, false, getClassLoader(files[0]));
+
+        // set each with object corresponding position
+        for (Coordinate po : pos) {
+            Object instance = classToLoad.newInstance();
+            Enemy act = (Enemy) instance;
+            act.setPosition(new Coordinate(po.getX(), po.getY()));
+            this.enemyClasses.add(act);
+        }
+
+    }
+
+    /**
+     * get the classloader with Privileged Blocks.
+     * @param file input
+     * @return classloader
+     */
+    public URLClassLoader getClassLoader(File file)  {
+        URLClassLoader child = (URLClassLoader) AccessController.doPrivileged(
+                new PrivilegedAction() {
+                    public Object run() {
+                        try {
+                            return new URLClassLoader(
+                                    new URL[]{file.toURI().toURL()},
+                                    Enemy.class.getClassLoader()
+                            );
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                });
+
+        return child;
+    }
+
+}
