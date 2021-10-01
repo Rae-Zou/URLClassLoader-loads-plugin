e4aa43a741fc93981603239b21c98581ece623cf
Rae-Zou <76859781+Rae-Zou@users.noreply.github.com>
2021-10-02 11:38:44 +1300

    Update LoadPluginFile.java

--- pluginDemoModule/src/LoadPluginFile.java
+++ pluginDemoModule/src/LoadPluginFile.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class LoadPluginFile {
 
-    private List<PluginInterface> pluginClasses = new ArrayList<>(); // store all bugs
+    private List<PluginInterface> pluginClasses = new ArrayList<>(); // store all plugins
 
     /**
      * Test the accessibility.
